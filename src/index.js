ffc7e4921616aac8059cdf06cef71c93ba6dba31
Carlos Ferreira <carlosprovisorio@icloud.com>
2017-03-04 16:05:57 -0500

    built functional component using props for VideoList

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyB0VXMelMtbWafZpVvyEdaEsw2aoU7tZzY';
 
 class App extends React.Component {
@@ -20,6 +21,7 @@ class App extends React.Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
