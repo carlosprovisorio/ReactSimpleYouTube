5590c74e13753806a3f2fb30d5019ebd9f70ea6f
Carlos Ferreira <carlosprovisorio@icloud.com>
2017-03-04 15:41:01 -0500

    define state in App component to render the the array of videos

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyB0VXMelMtbWafZpVvyEdaEsw2aoU7tZzY';
 
+class App extends React.Component {
 
-const App = () => {
-  return (
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
       <div>
         <SearchBar />
       </div>
-  )
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
