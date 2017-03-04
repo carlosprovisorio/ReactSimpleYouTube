10a361a3e5a64010058a9540345b928412904f3d
Carlos Ferreira <carlosprovisorio@icloud.com>
2017-03-04 12:15:05 -0500

    create package and add components files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyB0VXMelMtbWafZpVvyEdaEsw2aoU7tZzY'
+
+const App = () => {
+  return <div>Hi!</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
+
+
