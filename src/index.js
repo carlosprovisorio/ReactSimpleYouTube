a49c6bcf1c11cae3ec94c452092c0cce8d7aeb6e
Carlos Ferreira <carlosprovisorio@icloud.com>
2017-03-05 11:52:18 -0500

    add searching for videos

--- src/index.js
+++ src/index.js
@@ -16,8 +16,12 @@ class App extends React.Component {
       selectedVideo: null
     };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ 
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
         videos: videos,
         selectedVideo: videos[0]
       });
@@ -27,7 +31,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
